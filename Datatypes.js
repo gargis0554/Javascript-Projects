b6cdbb9af724dd605e682a2a6ca49a3b9316952c
Gargi shukla <124441226+gargis0554@users.noreply.github.com>
2024-11-14 22:39:47 +0000

    js commit

---
+++ Datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all js code as newer version
+// alert("hello")
+// alert(3+3) 
+let name="gargi"//string
+let age=20//number =>bigint
+let isLogginIN=false //bool
+//null-standalone value
+let state=null
+//undefined
+//symbol =>unique
+
+
+//object
+console.log(typeof age)
