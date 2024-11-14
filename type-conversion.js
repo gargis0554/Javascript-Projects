5a769cb2965ca02bf7f92b3ef0c6a546d0dac060
Gargi shukla <124441226+gargis0554@users.noreply.github.com>
2024-11-14 22:50:42 +0000

    js commit

---
+++ type-conversion.js
@@ -0,0 +1,10 @@
+let score="335"
+console.log(typeof score);
+let value=Number(score)
+console.log(typeof value)
+//type conversion
+//NaN=> Not a number string="123abc"=>undefined
+//0 null
+// bool=>1 or 0
+let bool=Boolean(score)
+console.log(typeof bool)
